433483e94aea2d63fd0ee74270f6c1872482e34d
Paul JOURDAIN <paul.jourdain@chroma.team>
2019-09-11 15:46:08 +0200

    Change default quality

--- www/image_resizer.js
+++ www/image_resizer.js
@@ -1,20 +1,25 @@
-var ImageResizer = function () {
-};
+var ImageResizer = function() {};
 
-ImageResizer.prototype.resize = function (options, success, fail) {
-    var defaultOptions = {
-        fileName: 'image_resizer_' + Date.now() + '.jpg',
-        quality: 100
-    };
+ImageResizer.prototype.resize = function(options, success, fail) {
+  var defaultOptions = {
+    fileName: "image_resizer_" + Date.now() + ".jpg",
+    quality: 90
+  };
 
-    options.fileName = options.fileName || defaultOptions.fileName;
-    options.quality = options.quality || defaultOptions.quality;
+  options.fileName = options.fileName || defaultOptions.fileName;
+  options.quality = options.quality || defaultOptions.quality;
 
-    cordova.exec(function (uri) {
-        success(uri);
-    }, function (e) {
-        fail(e);
-    }, "ImageResizer", "resize", [options]);
+  cordova.exec(
+    function(uri) {
+      success(uri);
+    },
+    function(e) {
+      fail(e);
+    },
+    "ImageResizer",
+    "resize",
+    [options]
+  );
 };
 
 var imageResizer = new ImageResizer();
